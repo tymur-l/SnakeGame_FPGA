126e0346ff9c7a8d246691c3306aed4665a5d54e
SgMakarov <42974693+SgMakarov@users.noreply.github.com>
2018-10-29 18:11:26 +0300

    use define instead of literals

--- src/modules/input/buttons.v
+++ src/modules/input/buttons.v
@@ -1,4 +1,4 @@
-//Include Defines
+include "../../definitions/define.vh"
 
 module buttons(
 input left, right, up, down,
@@ -7,30 +7,30 @@ output reg [1:0] out
 
 initial 
 begin 
-out = 2'b01;
+out = TOP_DIR;
 end
 	always@(*) // TODO: check whether it works without clk
 begin
 	if(left +right + up + down == 3'b001) // if only one pressed
 	begin
-		if(left && out != 2'b10) // TODO: Replace with defines
+		if(left && out != RIGHT_DIR) // TODO: Replace with defines
 		begin
-			out = 2'b00;
+			out = LEFT_DIR;
 		end
 		
-		if(right && out != 2'b00)
+		if(right && out != LEFT_DIR)
 		begin
-			out = 2'b10;
+			out = RIGHT_DIR;
 		end
 
-		if(up && out != 2'b11)
+		if(up && out != DOWN_DIR)
 		begin
-			out = 2'b01;
+			out = TOP_DIR;
 		end
 		
-		if(down && out != 2'b01)
+		if(down && out != TOP_DIR)
 		begin
-			out = 2'b11;
+			out = DOWN_DIR;
 		end
 
 	end
