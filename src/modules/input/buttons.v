c23a52d1a67ca636589ed01d14bcfc6caa5eeacf
Sergey Makarov <s.makarov@innopolis.university>
2018-10-29 17:37:06 +0300

    fix for buttons (output via output reg)

--- src/modules/input/buttons.v
+++ src/modules/input/buttons.v
@@ -1,31 +1,29 @@
 module buttons(
- input left, right, up, down
-); 
-reg [1:0] out; 
-initial
- 	begin
-		out = 2'b01; 
-	end 
-always@(*) 
+input left, right, up, down,
+output reg [1:0] out
+);
+
+initial 
+begin 
+out = 2'b01;
+end
+always@(*)
 begin
-	if(left +right + up + down == 1) 
-	begin 
-		if(left && ~(out == 2'b10))
-		begin 
-			out = 2'b00; 
-		end 
-		if(right && ~(out == 2'b00))
-		begin 
-			out = 2'b10; 
-		end 
-		if(up && ~(out == 2'b11))
-		begin
-		 	out = 2'b01;
-		end 
-		if(down && ~(out == 2'b01))
-		begin 
-			out = 2'b11; 
-		end 
-	end 
-end 
+if(left +right + up + down == 1) begin
+	if(left && out != 2'b10)begin
+		out = 2'b00;
+	end
+	if(right && out != 2'b00)begin
+		out = 2'b10;
+	end
+
+	if(up && out != 2'b11)begin
+		out = 2'b01;
+	end
+	if(down && out != 2'b01)begin
+		out = 2'b11;
+	end
+
+end
+end
 endmodule
