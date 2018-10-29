e9a99fa7f88c176033f2e358d7ec844fdf527494
SgMakarov <42974693+SgMakarov@users.noreply.github.com>
2018-10-29 18:13:26 +0300

    syntax fix:)

--- src/modules/input/buttons.v
+++ src/modules/input/buttons.v
@@ -7,30 +7,32 @@ output reg [1:0] out
 
 initial 
 begin 
-out = TOP_DIR;
+	out = `TOP_DIR;
 end
-	always@(*) // TODO: check whether it works without clk
+	
+	
+always@(*) // TODO: check whether it works without clk
 begin
 	if(left +right + up + down == 3'b001) // if only one pressed
 	begin
-		if(left && out != RIGHT_DIR) // TODO: Replace with defines
+		if(left && out != `RIGHT_DIR) // TODO: Replace with defines
 		begin
-			out = LEFT_DIR;
+			out = `LEFT_DIR;
 		end
 		
-		if(right && out != LEFT_DIR)
+		if(right && out != `LEFT_DIR)
 		begin
-			out = RIGHT_DIR;
+			out = `RIGHT_DIR;
 		end
 
-		if(up && out != DOWN_DIR)
+		if(up && out != `DOWN_DIR)
 		begin
-			out = TOP_DIR;
+			out = `TOP_DIR;
 		end
 		
-		if(down && out != TOP_DIR)
+		if(down && out != `TOP_DIR)
 		begin
-			out = DOWN_DIR;
+			out = `DOWN_DIR;
 		end
 
 	end
