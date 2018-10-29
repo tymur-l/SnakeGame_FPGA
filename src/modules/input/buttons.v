f73ab24a60495124c0229631f6e9a117b84e13cb
WinnerOK <danman339@gmail.com>
2018-10-29 17:58:43 +0300

    added some comments

--- src/modules/input/buttons.v
+++ src/modules/input/buttons.v
@@ -1,3 +1,5 @@
+//Include Defines
+
 module buttons(
 input left, right, up, down,
 output reg [1:0] out
@@ -7,23 +9,30 @@ initial
 begin 
 out = 2'b01;
 end
-always@(*)
+	always@(*) // TODO: check whether it works without clk
 begin
-	if(left +right + up + down == 3'b001) begin
-	if(left && out != 2'b10)begin
-		out = 2'b00;
-	end
-	if(right && out != 2'b00)begin
-		out = 2'b10;
-	end
+	if(left +right + up + down == 3'b001) // if only one pressed
+	begin
+		if(left && out != 2'b10) // TODO: Replace with defines
+		begin
+			out = 2'b00;
+		end
+		
+		if(right && out != 2'b00)
+		begin
+			out = 2'b10;
+		end
 
-	if(up && out != 2'b11)begin
-		out = 2'b01;
-	end
-	if(down && out != 2'b01)begin
-		out = 2'b11;
-	end
+		if(up && out != 2'b11)
+		begin
+			out = 2'b01;
+		end
+		
+		if(down && out != 2'b01)
+		begin
+			out = 2'b11;
+		end
 
-end
+	end
 end
 endmodule
