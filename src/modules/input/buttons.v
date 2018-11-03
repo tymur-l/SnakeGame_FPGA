8d421c5dda4afda14d2881c6a07f86454593e4a3
Sergey Makarov <s.makarov@innopolis.university>
2018-11-03 15:30:51 +0300

    new input module

--- src/modules/input/buttons.v
+++ src/modules/input/buttons.v
@@ -1,40 +1,74 @@
-include "../../definitions/define.vh"
 
-module buttons(
-input left, right, up, down,
-output reg [1:0] out
+`include "../../definitions/define.vh"
+
+module buttons
+(
+	input one_resisor_x, two_resistors_x, one_resisor_y, two_resistors_y, clk,
+	output reg [0:1] direction
+
 );
 
-initial 
-begin 
-	out = `TOP_DIR;
-end
-	
-	
-always@(*) // TODO: check whether it works without clk
+reg left, right, up, down;
+
+initial
+
 begin
-	if(left +right + up + down == 3'b001) // if only one pressed
-	begin
-		if(left && out != `RIGHT_DIR) // TODO: Replace with defines
-		begin
-			out = `LEFT_DIR;
-		end
-		
-		if(right && out != `LEFT_DIR)
-		begin
-			out = `RIGHT_DIR;
-		end
-
-		if(up && out != `DOWN_DIR)
-		begin
-			out = `TOP_DIR;
-		end
-		
-		if(down && out != `TOP_DIR)
-		begin
-			out = `DOWN_DIR;
-		end
-
-	end
+    direction = `TOP_DIR;
 end
+
+
+
+always @(posedge clk)
+
+begin
+
+    	left = two_resistors_x;
+	right = ~one_resistor_x;
+	up = two_resistors_y;
+	down = ~one_resistor_y;
+
+	// u=up;
+	// l=left;
+	// d=down;
+	// r=right;
+
+	if(left +right + up + down == 3'b001) // if only one pressed 
+
+	begin 
+
+		if(left && direction != `RIGHT_DIR) 
+
+		begin 
+			direction = `LEFT_DIR; 
+		end 
+
+
+
+		if(right && direction != `LEFT_DIR) 
+
+		begin 
+			direction = `RIGHT_DIR; 
+		end 
+
+
+
+		if(up && direction != `DOWN_DIR) 
+
+		begin 
+			direction = `UP_DIR; 
+		end 			 
+
+
+
+		if(down && direction != `UP_DIR) 
+
+		begin 
+			direction = `DOWN_DIR; 
+		end 
+
+	end 
+
+end 
+
+
 endmodule
